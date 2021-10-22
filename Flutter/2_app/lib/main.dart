a09e623e68c269295e3396ee9c05249c15f83ba4
Quillian <quillian.chardon@synerg-in.com>
2021-10-22 10:21:20 +0200

    ajout de la barre de recherche

--- Flutter/2_app/lib/main.dart
+++ Flutter/2_app/lib/main.dart
@@ -26,7 +26,14 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: MyAppBar(),
-      body: Container(),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SearchSection(),
+            HotelSection(),
+          ],
+        ),
+      ),
     );
   }
 }
@@ -65,6 +72,93 @@ class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
       ],
       centerTitle: true,
       backgroundColor: Colors.white,
+    );
+  }
+}
+
+class SearchSection extends StatelessWidget {
+  const SearchSection({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 200,
+      color: Colors.grey[200],
+      padding: const EdgeInsets.fromLTRB(10, 25, 10, 10),
+      child: Column(children: [
+        Row(
+          children: [
+            Expanded(
+              child: Container(
+                padding: const EdgeInsets.only(left: 5),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(30),
+                  boxShadow: const [
+                    BoxShadow(
+                      color: Colors.grey,
+                      blurRadius: 4,
+                      offset: Offset(0, 3),
+                    ),
+                  ],
+                ),
+                child: const TextField(
+                  decoration: InputDecoration(
+                    hintText: 'London',
+                    contentPadding: EdgeInsets.all(10),
+                    border: InputBorder.none,
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(width: 10),
+            Container(
+              height: 50,
+              width: 50,
+              decoration: const BoxDecoration(
+                color: Colors.green,
+                borderRadius: BorderRadius.all(Radius.circular(25)),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey,
+                    blurRadius: 4,
+                    offset: Offset(0, 4),
+                  ),
+                ],
+              ),
+              child: ElevatedButton(
+                onPressed: () {},
+                child: Icon(
+                  Icons.search,
+                  size: 26,
+                ),
+                style: ElevatedButton.styleFrom(
+                  shape: CircleBorder(),
+                  padding: EdgeInsets.all(10),
+                  primary: Colors.greenAccent[200],
+                ),
+              ),
+            ),
+          ],
+        ),
+        const SizedBox(height: 50),
+        Container(
+          height: 50,
+          color: Colors.red,
+        ),
+      ]),
+    );
+  }
+}
+
+class HotelSection extends StatelessWidget {
+  const HotelSection({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 1500,
+      color: Colors.white,
     );
   }
 }
